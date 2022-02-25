b38a16bc0f127bff535940d4397e9124be1f49b1
Kamino <iden1107@icloud.com>
2022-02-25 21:54:32 +0900

    initial commit

--- pages/index.vue
+++ pages/index.vue
@@ -5,12 +5,26 @@
         <NuxtLogo />
         <VuetifyLogo />
       </v-card>
+    <p>{{posts[0]}}</p>
     </v-col>
   </v-row>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  methods:{
+
+  },
+  async asyncData({ $axios }) {
+			// 取得先のURL
+			const url = "https://jsonplaceholder.typicode.com/users";
+			// リクエスト（Get）
+			const response = await $axios.$get(url);
+			// 配列で返ってくるのでJSONにして返却
+			return {
+				posts: response
+			};
+		}
 }
 </script>
